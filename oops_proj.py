e22acaaebe6f7df609fcbf2ddaa5bc69d72587db
Sachit Khade <sachitkhade1204@gmail.com>
2025-11-27 04:40:01 +0530

    project completed

--- oops_proj.py
+++ oops_proj.py
@@ -10,15 +10,17 @@ class chatbook:
                          2. press 2 for signIN
                          3. press 3 for writing a post
                          4. press 4 for messaging a friend
-                         5. Any key to exit ''')
+                         5. Any key to exit
+                         
+                         ----> ''')
         if user_input=='1':
             self.signup()
         elif user_input=='2':
             self.signin()
         elif user_input=='3':
-            pass
+            self.my_post()
         elif user_input=='4':
-            pass
+            self.send_msg()
         else:
             exit()
     def signup(self):
@@ -32,7 +34,6 @@ class chatbook:
     def signin(self):
         if self.username=='' and self.username=='':
             print("please signup first by pressing 1 in the menu")
-            self.menu()
         else:
             uname=input("enter your email adress here ->")
             pwd2=input("enter your password here -> ")
@@ -43,6 +44,23 @@ class chatbook:
                 print("above entered credentials are wrong , plz write the correct credential")
         print("\n")
         self.menu()
-            
+    def my_post(self):
+        if self.loggedin==True :
+            txt=input("whats on your mind lets post it out ")
+            print(f"the post has been public--->{txt}")
+        else:
+            print("plz signup and signin first and proceed further for chatbook exprience")
+        print("\n")
+        self.menu()
+    def send_msg(self):
+        if self.loggedin==True :
+            txt=input("write the message here---> ")
+            frd=input("whom do you want to send this message--->")
+            print(f"message sent to--->{frd}")
+        else:
+            print("plz signup and signin first and proceed further for chatbook exprience")
+        print("\n")
+        self.menu()
 
-obj=chatbook()
+#obj=chatbook()
+#just remove the above commment to run this file
